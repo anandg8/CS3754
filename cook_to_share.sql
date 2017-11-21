6473738c98bf799f21d4b14636b3ad2182f74071
adayal <adayal@vt.edu>
2017-11-21 16:21:58 -0500

    making changes to sql database

--- cook_to_share.sql
+++ cook_to_share.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2017 at 06:27 PM
+-- Generation Time: Nov 21, 2017 at 09:20 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -30,6 +30,7 @@ CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL COMMENT 'user_id of the commenter',
   `reply_id` int(11) NOT NULL COMMENT 'reply_id, Foreign key that points back to this table. Field can be left blank',
+  `comment_table_id` int(11) NOT NULL,
   `date_posted` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `is_visible` int(11) NOT NULL DEFAULT '1' COMMENT 'Is the comment visible or ''deleted''',
   `vote_id` int(11) NOT NULL COMMENT 'Points to the vote id on the vote table',
@@ -39,14 +40,40 @@ CREATE TABLE `comments` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cuisine_table`
+-- Table structure for table `comments_conjunction`
 --
 
-CREATE TABLE `cuisine_table` (
+CREATE TABLE `comments_conjunction` (
+  `id` int(11) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments_conjunction`
+--
+
+INSERT INTO `comments_conjunction` (`id`, `date_created`) VALUES
+(1, '2017-11-21 13:18:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cuisine_type`
+--
+
+CREATE TABLE `cuisine_type` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL COMMENT 'Name of Cusine',
   `description` varchar(500) NOT NULL COMMENT 'Description of Cusine'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cuisine_type`
+--
+
+INSERT INTO `cuisine_type` (`id`, `name`, `description`) VALUES
+(1, 'Chinese', 'Traditional Chinese Cooking'),
+(2, 'Indian', 'Traditional Indian Cooking');
 
 -- --------------------------------------------------------
 
@@ -58,6 +85,7 @@ CREATE TABLE `dish` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL COMMENT 'user_id Foreign Key',
   `cuisine_id` int(11) NOT NULL COMMENT 'cuisine_id Foreign Key',
+  `dish_name` varchar(15) NOT NULL,
   `description` text NOT NULL COMMENT 'description of dish',
   `reservation_time` datetime NOT NULL COMMENT 'reservation time',
   `meal_time` datetime NOT NULL COMMENT 'meal time, != reservation time',
@@ -65,6 +93,13 @@ CREATE TABLE `dish` (
   `num_guests` int(10) UNSIGNED NOT NULL COMMENT 'Number of Guests: Cannot be negative',
   `comments_id` int(11) NOT NULL COMMENT 'comments_key Foreign Key'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dish`
+--
+
+INSERT INTO `dish` (`id`, `user_id`, `cuisine_id`, `dish_name`, `description`, `reservation_time`, `meal_time`, `cost`, `num_guests`, `comments_id`) VALUES
+(1, 1, 1, 'General Tso', 'Chicken Dish\r\nVery Healthy', '2017-11-28 15:00:00', '2017-11-28 18:00:00', 5.5, 5, 1);
 
 -- --------------------------------------------------------
 
@@ -100,6 +135,14 @@ CREATE TABLE `user` (
   `username` varchar(20) NOT NULL COMMENT 'Username for User',
   `password` varchar(30) NOT NULL COMMENT 'Password for User - Hashed',
   `email` varchar(20) NOT NULL COMMENT 'Email for User',
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `address` varchar(60) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `state` varchar(2) NOT NULL,
+  `zipcode` int(5) NOT NULL,
+  `security_question` varchar(100) NOT NULL,
+  `security_answer` varchar(100) NOT NULL,
   `user_permission` int(11) NOT NULL DEFAULT '0' COMMENT '0 - Regular User, 1 - Admin',
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'When the user was created',
   `is_active` int(11) NOT NULL DEFAULT '1' COMMENT 'Did the user deactivate/Delete their account',
@@ -107,6 +150,13 @@ CREATE TABLE `user` (
   `location` varchar(30) NOT NULL COMMENT 'long and lat',
   `user_rating` double NOT NULL COMMENT 'Averaged rating'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `address`, `city`, `state`, `zipcode`, `security_question`, `security_answer`, `user_permission`, `date_created`, `is_active`, `profile_picture`, `location`, `user_rating`) VALUES
+(1, 'adayal', 'password', 'adayal@vt.edu', 'Amit', 'Dayal', '123 fake street', 'Blacksburg', 'VA', 24060, 'What is your favorite color?', 'Hot Pink', 0, '2017-11-21 12:53:50', 1, 'default.png', '-77.4310990,38.8942790', 3);
 
 -- --------------------------------------------------------
 
@@ -132,12 +182,20 @@ CREATE TABLE `votes` (
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `comments_vote_id` (`vote_id`),
-  ADD KEY `comments_user_id` (`user_id`);
+  ADD KEY `comments_user_id` (`user_id`),
+  ADD KEY `comment_table_id` (`comment_table_id`),
+  ADD KEY `vote_id` (`vote_id`);
 
 --
--- Indexes for table `cuisine_table`
+-- Indexes for table `comments_conjunction`
 --
-ALTER TABLE `cuisine_table`
+ALTER TABLE `comments_conjunction`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cuisine_type`
+--
+ALTER TABLE `cuisine_type`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -188,20 +246,25 @@ ALTER TABLE `votes`
 ALTER TABLE `comments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `cuisine_table`
+-- AUTO_INCREMENT for table `comments_conjunction`
 --
-ALTER TABLE `cuisine_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `comments_conjunction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `cuisine_type`
+--
+ALTER TABLE `cuisine_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `dish`
 --
 ALTER TABLE `dish`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key for Users';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key for Users', AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `votes`
 --
@@ -215,16 +278,16 @@ ALTER TABLE `votes`
 -- Constraints for table `comments`
 --
 ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`vote_id`) REFERENCES `votes` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_3` FOREIGN KEY (`comment_table_id`) REFERENCES `comments_conjunction` (`id`);
 
 --
 -- Constraints for table `dish`
 --
 ALTER TABLE `dish`
-  ADD CONSTRAINT `dish_ibfk_1` FOREIGN KEY (`comments_id`) REFERENCES `comments` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `dish_ibfk_2` FOREIGN KEY (`cuisine_id`) REFERENCES `cuisine_table` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `dish_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `dish_ibfk_2` FOREIGN KEY (`cuisine_id`) REFERENCES `cuisine_type` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `dish_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `dish_ibfk_4` FOREIGN KEY (`comments_id`) REFERENCES `comments_conjunction` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `friends`
@@ -244,7 +307,6 @@ ALTER TABLE `guest_list`
 -- Constraints for table `votes`
 --
 ALTER TABLE `votes`
-  ADD CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`comments_id`) REFERENCES `comments` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `votes_ibfk_3` FOREIGN KEY (`dish_id`) REFERENCES `dish` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
