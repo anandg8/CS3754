9ed9772190d6a0f66b0e9c669462c2342da0082e
Anand Guruswamy <anandg8@vt.edu>
2017-12-13 02:33:58 -0500

    Chat example is working

---
+++ src/java/com/mycompany/managers/MessageManagerLocal.java
@@ -0,0 +1,22 @@
+/*
+ * Created by Ananthavel Guruswamy on 2017.12.13  * 
+ * Copyright © 2017 Ananthavel Guruswamy. All rights reserved. * 
+ */
+package com.mycompany.managers;
+
+import java.util.Date;
+import javax.ejb.Local;
+import com.mycompany.EntityBeans.Message;
+ 
+/**
+ * 
+ * @author Anand
+ */
+@Local
+public interface MessageManagerLocal {
+ 
+    void sendMessage(Message msg);
+ 
+    Message getFirstAfter(Date after);
+ 
+}
