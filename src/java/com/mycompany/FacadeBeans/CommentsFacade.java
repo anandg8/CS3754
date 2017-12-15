b3444f61b3807e9cd81c35163c50b3d0c1ee3b29
adayal <adayal@vt.edu>
2017-12-14 20:22:26 -0500

    adding comments

--- src/java/com/mycompany/FacadeBeans/CommentsFacade.java
+++ src/java/com/mycompany/FacadeBeans/CommentsFacade.java
@@ -30,6 +30,11 @@ public class CommentsFacade extends AbstractFacade<Comments> {
         super(Comments.class);
     }
     
+    /**
+     * Get all the comments from the database where the comment_table_id is the comments_conjunction_id
+     * @param comments_conjunction_id of the dish relating to this forum board
+     * @return a list of comments
+     */
     public List<Comments> getCommentsRecursively(int comments_conjunction_id) {
         Query q = getEntityManager().createNativeQuery("SELECT * FROM comments WHERE comment_table_id = ?", Comments.class);
         q.setParameter(1, comments_conjunction_id);
@@ -37,7 +42,15 @@ public class CommentsFacade extends AbstractFacade<Comments> {
         return parentComments;
     }
 
-    //id is the comment id
+    /**
+     * Get the score or 'points' for the comment.
+     * This will return a list of 'actions' which are are labeled as such:
+     *      -1 (negative score)
+     *      +1 (positive score)
+     * Adding all these scores across all users gives us the final score
+     * @param id of the comments id
+     * @return the score of a comment
+     */
     public int getScore(int id) {
         List<Integer> actionList = getEntityManager().createNativeQuery("SELECT action FROM votes WHERE comments_id = ?").setParameter(1, id).getResultList();
         //if -1, downvote, if 1, upvote
@@ -48,11 +61,25 @@ public class CommentsFacade extends AbstractFacade<Comments> {
         return score;
     }
 
+    /**
+     * Check if the user voted for a comment. We check this by counting the number of rows in the votes table
+     * cross-referenced with the user_id and comments_id. If the number is equal to 1, we know that the user
+     * has already voted for this comment
+     * @param userid to check for
+     * @param commentID to check again
+     * @return true if voted for, false otherwise
+     */
     public boolean didUserVote(Integer userid, int commentID) {
         Long i = (Long) getEntityManager().createNativeQuery("SELECT COUNT(*) FROM votes WHERE user_id = ? AND comments_id = ?").setParameter(1, userid).setParameter(2, commentID).getSingleResult();
         return i.intValue() == 1;
     }
 
+    /**
+     * Upvote a comment
+     * @param userid that will upvote
+     * @param commentid that the user will upvote
+     * @param dishid of the associated comment
+     */
     public void userUpvote(Integer userid, int commentid, int dishid) {
         getEntityManager().createNativeQuery("INSERT INTO votes VALUES(DEFAULT, ?, 1, ?, ?)")
                 .setParameter(1, userid)
@@ -61,6 +88,12 @@ public class CommentsFacade extends AbstractFacade<Comments> {
                 .executeUpdate();
     }
     
+    /**
+     * Downvote a comment
+     * @param userid that will upvote
+     * @param commentid that the user will upvote
+     * @param dishid of the associated comment
+     */
     public void userDownvote(Integer userid, int commentid, int dishid) {
         getEntityManager().createNativeQuery("INSERT INTO votes VALUES(DEFAULT, ?, -1, ?, ?)")
                 .setParameter(1, userid)
@@ -69,6 +102,12 @@ public class CommentsFacade extends AbstractFacade<Comments> {
                 .executeUpdate();
     }
 
+    /**
+     * Submit a comment
+     * @param userid of the poster
+     * @param commentsConjunctionID that this post relates to the selected dish
+     * @param comment itself to store
+     */
     public void submitComment(int userid, int commentsConjunctionID, String comment) {
         getEntityManager().createNativeQuery("INSERT INTO comments VALUES(DEFAULT, ?, 0, ?, DEFAULT, 1, ?)")
                 .setParameter(1, userid)
--- src/java/com/mycompany/FacadeBeans/DishFacade.java
+++ src/java/com/mycompany/FacadeBeans/DishFacade.java
@@ -1,6 +1,6 @@
 /*
- * Created by Osman Balci on 2017.11.21  * 
- * Copyright © 2017 Osman Balci. All rights reserved. * 
+ * Created by Amit Dayal on 2017.12.12  * 
+ * Copyright © 2017 Amit Dayal. All rights reserved. * 
  */
 package com.mycompany.FacadeBeans;
 
@@ -29,6 +29,14 @@ public class DishFacade extends AbstractFacade<Dish> {
         super(Dish.class);
     }
     
+    /**
+     * Creates a dish. This process is a little bit complicated due to the foreign key checks.
+     * Essentially the comments_conjunction needs to be created first before the dish itself.
+     * There is no great way to get the insert id so we first create the comment_conjunction,
+     * then we fetch the MAX(id) which is the last id inserted. We use that id in the dish
+     * and insert that into the database
+     * @param dish object to insert
+     */
     public void createDish(Dish dish) {
         getEntityManager().createNativeQuery("INSERT INTO comments_conjunction VALUES(DEFAULT, DEFAULT)").executeUpdate();
         Integer b = (Integer)getEntityManager().createNativeQuery("SELECT MAX(id) FROM comments_conjunction").getSingleResult();
@@ -46,10 +54,23 @@ public class DishFacade extends AbstractFacade<Dish> {
                 .executeUpdate();
     }
     
+    /**
+     * Get the current number of reservations for a certain dish
+     * This information is stored in the 'guest_list' table, which acts as a conjunction table
+     * for the users and the dishes
+     * @param dishID to search for
+     * @return the number of current users signed up for a dish
+     */
     public int getCurrentNumReservations(int dishID) {
         return ((Long)getEntityManager().createNativeQuery("SELECT COUNT(*) FROM guest_list WHERE dish_id = ?").setParameter(1, dishID).getSingleResult()).intValue();
     }
 
+    /**
+     * Check if the user is a guest (i.e. has already signed up for) for this dish
+     * @param userid to check for
+     * @param dishid to check for
+     * @return true if guest, false otherwise
+     */
     public boolean isGuest(int userid, int dishid) {
         int count = ((Long)getEntityManager().createNativeQuery("SELECT COUNT(*) FROM guest_list WHERE user_id = ? AND dish_id = ?")
                 .setParameter(1, userid)
@@ -59,6 +80,11 @@ public class DishFacade extends AbstractFacade<Dish> {
             
     }
 
+    /**
+     * Reserve dishes for a user. This added the user to the guest_list
+     * @param dishid to reserve
+     * @param userid to add to the guest list
+     */
     public void reserveDish(int dishid, int userid) {
         getEntityManager().createNativeQuery("INSERT INTO guest_list VALUES(?, ?, \"\")")
                 .setParameter(1, userid)
@@ -66,6 +92,11 @@ public class DishFacade extends AbstractFacade<Dish> {
                 .executeUpdate();
     }
     
+    /**
+     * Remove user from the guest list
+     * @param dishid to unreserve
+     * @param userid to remove from the guest list
+     */
     public void unreserveDish(int dishid, int userid) {
         getEntityManager().createNativeQuery("DELETE FROM guest_list WHERE dish_id = ? AND user_id = ?")
                 .setParameter(1, dishid)
@@ -74,6 +105,17 @@ public class DishFacade extends AbstractFacade<Dish> {
         
     }
 
+    /**
+     * Return a list of dishes in the future that are held by a current user's guest subscription
+     * This uses a complex SQL query by using a JOIN. We can join the dish id on the guest_list.
+     * This creates a larger table where we can query the data more easily. In this case, we create
+     * a Dish object using each row retrieved from the database. We know a reservation is current
+     * using a comparison in SQL.
+     * This has faster execution since this runs directly on SQL. The reverse case would be to pull
+     * all the dishes and then manually search through in Java.
+     * @param userid to search for
+     * @return a list of dishes
+     */
     public List<Dish> findCurrentReservationsByUser(int userid) {
         List<Dish> list = getEntityManager().createNativeQuery("SELECT dish.* FROM dish INNER JOIN guest_list ON dish.id = guest_list.dish_id WHERE (guest_list.user_id = ? AND CURRENT_TIMESTAMP < dish.reservation_time)", Dish.class)
                 .setParameter(1, userid)
@@ -81,6 +123,12 @@ public class DishFacade extends AbstractFacade<Dish> {
         return list;
     }
     
+    /**
+     * Return all the past reservations of a user. We can fetch this data using the guest list to join on.
+     * We know a dish is in the past by comparing the current time to the timestamp in the database
+     * @param userid
+     * @return 
+     */
     public List<Dish> findPastReservationsByUser(int userid) {
         List<Dish> list = getEntityManager().createNativeQuery("SELECT dish.* FROM dish INNER JOIN guest_list ON dish.id = guest_list.dish_id WHERE (guest_list.user_id = ? AND CURRENT_TIMESTAMP > dish.meal_time)", Dish.class)
                 .setParameter(1, userid)
@@ -88,8 +136,12 @@ public class DishFacade extends AbstractFacade<Dish> {
         return list;
     }
     
-    public List<Dish> findCurrentUserDishes(int userID)
-    {
+    /**
+     * Find the list of user dishes
+     * @param userID to search for
+     * @return a list of all the dishes
+     */
+    public List<Dish> findCurrentUserDishes(int userID) {
         List<Dish> userDishList = getEntityManager().createNativeQuery("SELECT dish.* FROM dish INNER JOIN user ON dish.user_id = user.id WHERE (user.id = ?)", Dish.class)
                 .setParameter(1, userID)
                 .getResultList();
@@ -97,6 +149,11 @@ public class DishFacade extends AbstractFacade<Dish> {
         return userDishList;
     }
     
+    /**
+     * Get the comments conjunction id of the dish. This will link the dish to the comments table
+     * @param dishid to search for
+     * @return the comments table id
+     */
     public int getCommentsConjunctionID(int dishid) {
         int id = ((Integer)getEntityManager().createNativeQuery("SELECT comments_id FROM dish WHERE id = ?")
                 .setParameter(1, dishid)
